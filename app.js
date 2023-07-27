97962f632271fef7b9ab70c5ac0e5fce84a63e69
unknown <tbrandalessi@gmail.com>
2023-07-27 17:42:26 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPaht = path.resolve(__dirname, './public');
+app.use( express.static(publicPaht) );
+
+app.listen(3000, () =>{
+    console.log("Levantando un servidor con Express")
+});
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+
+});
+
+app.get("/register",(req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get("/login",(req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});

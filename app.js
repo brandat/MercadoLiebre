3cd51354aee78cfe477084c0ed35602a6e730bf8
unknown <tbrandalessi@gmail.com>
2023-07-27 18:06:25 -0300

    cambios del servidor

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 const publicPaht = path.resolve(__dirname, './public');
 app.use( express.static(publicPaht) );
 
+const port = ptocess.env.PORT || 3000;
+app.listen(port, () => console.log('servidor corriendo en el puerto ${port}'));
+app.use (expres.static("public"));
+
 app.listen(3000, () =>{
     console.log("Levantando un servidor con Express")
 });
